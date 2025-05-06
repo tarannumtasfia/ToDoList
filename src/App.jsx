1ba1171636306adeaa8d84e69978faaba7a4cab7
tarannumtasfia <tasfiatarannum822.com>
2025-05-06 11:35:25 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './index.css';
+import Todo from './components/Todo';
+const App = () => {
+  return (
+    <div className='bg-stone-900 grid py-4 min-h-screen'><Todo/></div>
+  )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css' // Import the CSS file with Tailwind styles
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
