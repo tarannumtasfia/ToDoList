c735986ddd09c9f6a2c93d0131cd40c1a68dc19b
tarannumtasfia <tasfiatarannum@yahoo.com>
2025-07-15 11:55:33 +0600

    commited

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -43,7 +43,7 @@ useEffect(()=>{
 }
 ,[todoList])
   return (
-    <div className='bg-white place-self-center w-11/12 max-w-md flex flex-col p-7 min-h-[350px] rounded-xl'>
+    <div className='bg-white place-self-center w-11/12 max-w-md flex flex-col p-7 min-h-[150px] rounded-xl'>
       <div className='flex items-center mt-7 gap-2'>
         <img className="w-8" src={todo_icon} alt="" />
         <h1 className='text-3xl font-semibold'>To-Do List</h1>
