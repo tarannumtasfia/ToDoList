bd75eba39269f677757400c1febc04a6efe7b00b
tarannumtasfia <tasfiatarannum@yahoo.com>
2025-07-15 11:52:04 +0600

    commited

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+
 export default defineConfig({
+  base: '/ToDoList/', // Add this line for GitHub Pages deployment
   plugins: [
     react(),
     tailwindcss()
